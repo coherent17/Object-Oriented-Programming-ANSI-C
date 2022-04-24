4bcbb4b05fb251f9153c710858f6ba5e0943c8d9
coherent <mnb51817@gmail.com>
2022-04-24 21:53:39 +0800

    abstract data type - bags

---
+++ 1 Abstract Data Type/Bag/Object.h
@@ -0,0 +1,7 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+extern const void *Object;
+int differ(const void *a, const void *b);
+
+#endif
---
+++ 1 Abstract Data Type/Bag/Set.h
@@ -0,0 +1,13 @@
+#ifndef SET_H
+#define SET_H
+
+extern const void *Set;
+
+//using const void * so that the poniter can point to any type, 
+//but we can't change the object that point to because of "const"
+void *add(void *set, const void *element);
+void *find(const void *set, const void *element);
+void *drop(void *set, const void *element);
+int contains(const void *set, const void *element);
+
+#endif
